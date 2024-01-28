34f66afc079d25f332bc323e0d109c777e97f757
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2024-01-28 15:23:59 +0530

    Update callbacks.py

--- bot/callbacks.py
+++ bot/callbacks.py
@@ -950,6 +950,7 @@ async def vbr_callback(event, txt, user_id, chat_id):
                             return
                 await saveoptions(user_id, 'use_vbr', eval(new_position), SAVE_TO_DATABASE)
                 await event.answer(f"❤ VBR 🖤 - {str(new_position)}")
+
             elif txt.startswith("crfset"):
                 if eval(new_position):
                         crf = await get_crf(chat_id, user_id, event, 120, "Send CRF Value")
